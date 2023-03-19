beebffcf4576e1b7cb148f7f9d0bf5effa58a2be
terry <apple7235266@gmail.com>
2023-03-19 23:01:28 +0800

    新增主畫面跳轉至各畫面

--- app/src/main/java/com/example/myapplication/Start.kt
+++ app/src/main/java/com/example/myapplication/Start.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,7 +12,44 @@ class Start : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val fight :ImageButton = findViewById(R.id.fight)
+        val history :ImageButton = findViewById(R.id.history)
+        val shop :ImageButton = findViewById(R.id.shop)
+        val backPack :ImageButton = findViewById(R.id.backPack)
 
+        fight.setOnClickListener {
+            entryFight()
+        }
+        history.setOnClickListener {
+            entryHistory()
+        }
+        shop.setOnClickListener {
+            entryShop()
+        }
+        backPack.setOnClickListener {
+            entryBackPack()
+        }
+
+    }
+    private fun entryFight(){
+        val intent = Intent(this, Fight::class.java)
+        // 啟動新的 Activity
+        startActivity(intent)
+    }
+    private fun entryHistory(){
+        val intent = Intent(this, History::class.java)
+        // 啟動新的 Activity
+        startActivity(intent)
+    }
+    private fun entryShop(){
+        val intent = Intent(this, Shop::class.java)
+        // 啟動新的 Activity
+        startActivity(intent)
+    }
+    private fun entryBackPack(){
+        val intent = Intent(this, BackPack::class.java)
+        // 啟動新的 Activity
+        startActivity(intent)
     }
 
 }
