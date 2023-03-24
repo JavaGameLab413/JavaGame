39374fa2f100c6aa0f1e431eda06a4ff95561265
BoBo <bensonrohe67@gmail.com>
2023-03-24 14:57:05 +0800

    主會面新增登入按鈕 點擊傳送至登入畫面

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //將畫面設定為按鈕
         val entry: ImageButton = findViewById(R.id.put_data)
+        val btLogin = findViewById<Button>(R.id.buttonLogin2)
         //朝畫面點擊後切換畫面
         entry.setOnClickListener {
             // 執行xml檔
             val intent = Intent(this, Start::class.java)
             // 啟動新的 Activity
+            startActivity(intent)
+        }
+
+        btLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
 
