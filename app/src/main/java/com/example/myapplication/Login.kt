9af9cf34a7ca21c6591c1f00721984481bafbeaf
Terry <apple7235266@gmail.com>
2023-04-22 06:03:26 +0000

    Revert "Merge branch 'feat-add-delete-account-function' into 'main'"
    This reverts merge request !9

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -12,19 +12,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class Login : AppCompatActivity() {
 
-    private val userDatabaseCollectionName = "users"
-    private val propertysDatabaseCollectionName = "propertys"
-    private val userDatabaseAccountField = "account"
-    private val userDatabasePasswordField = "password"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         //按鈕
         val login = findViewById<Button>(R.id.ButtonLogin)
-        val delete = findViewById<Button>(R.id.ButtonDeleteAccount)
+        val change = findViewById<Button>(R.id.ButtonChange)
         val add = findViewById<Button>(R.id.ButtonAdd)
-
         //輸入的文字框(帳號密碼)
         val inputAccount = findViewById<EditText>(R.id.InputAccount)
         val inputPassword = findViewById<EditText>(R.id.InputPassword)
@@ -32,21 +27,21 @@ class Login : AppCompatActivity() {
         // Access Firebase Firestorm
         val db = FirebaseFirestore.getInstance()
         // Create a new document with a generated ID
-        db.collection(userDatabaseCollectionName).document()
-        val readDocRed = db.collection(userDatabaseCollectionName)
+        db.collection("users").document()
+        val readDocRed = db.collection("users")
 
         //設置登入按鈕功能
         login.setOnClickListener {
-            //Log.d("test", inputAccount.text.toString())
-            readDocRed.whereEqualTo(userDatabaseAccountField , inputAccount.text.toString()).get()
+            Log.d("test", inputAccount.text.toString())
+            readDocRed.whereEqualTo("account", inputAccount.text.toString()).get()
                 .addOnSuccessListener { documents ->
                     if (documents.size() > 0) {
                         // 找到使用者，檢查密碼
                         val user = documents.first()
-                        val password = user.getString(userDatabasePasswordField)
+                        val password = user.getString("password")
                         if (password == inputPassword.text.toString()) {
                             // 密碼正確，登錄成功
-                            Toast.makeText(this, "登入成功!", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this,"登入成功!",Toast.LENGTH_SHORT).show()
                             Log.d(TAG, "Login success!")
                             //切換畫面
                             val intent = Intent(this, Start::class.java)
@@ -60,55 +55,18 @@ class Login : AppCompatActivity() {
 
                         } else {
                             // 密碼錯誤
-                            Toast.makeText(this, "登入失敗!", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this,"登入失敗!",Toast.LENGTH_SHORT).show()
                             Log.d(TAG, "Incorrect password!")
                         }
                     } else {
                         // 找不到使用者
-                        Toast.makeText(this, "登入失敗!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this,"登入失敗!",Toast.LENGTH_SHORT).show()
                         Log.d(TAG, "User not found!")
                     }
                 }
                 .addOnFailureListener { exception ->
                     // 讀取資料失敗
                     Log.w(TAG, "Error getting documents.", exception)
-                }
-        }
-        //新增帳號功能按鈕監聽
-        add.setOnClickListener {
-            val intent = Intent(this, Signup::class.java)
-            startActivity(intent)
-        }
-        //刪除帳號功能按鈕監聽
-        delete.setOnClickListener {
-            val userAccount = inputAccount.text.toString()
-            val userPassword = inputPassword.text.toString()
-
-            val query = FirebaseFirestore.getInstance().collection(userDatabaseCollectionName)
-                .whereEqualTo(userDatabaseAccountField ,userAccount)
-                .whereEqualTo(userDatabasePasswordField ,userPassword)
-
-            query.get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        // 刪除符合條件的文檔
-                        document.reference.delete()
-                            .addOnSuccessListener {
-                                FirebaseFirestore.getInstance().collection(propertysDatabaseCollectionName)
-                                    .document(document.id)
-                                    .delete()
-                                Toast.makeText(this, "已刪除資料", Toast.LENGTH_SHORT).show()
-                            }
-                            .addOnFailureListener { e ->
-                                Log.e(TAG, "刪除資料失敗: ", e)
-                                Toast.makeText(this, "刪除資料失敗", Toast.LENGTH_SHORT).show()
-                            }
-
-                    }
-                }
-                .addOnFailureListener { e ->
-                    Log.e(TAG, "查無此帳號: ", e)
-                    Toast.makeText(this, "查詢資料失敗", Toast.LENGTH_SHORT).show()
                 }
         }
         //新增帳號功能按鈕監聽
