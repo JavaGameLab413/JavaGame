61c269b32b120253be27384803eb4f6e7abd3acd
terry <apple7235266@gmail.com>
2023-05-15 22:26:23 +0800

    style:更改文字樣式

--- app/src/main/java/com/example/myapplication/FightSelect.kt
+++ app/src/main/java/com/example/myapplication/FightSelect.kt
@@ -4,44 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.firebase.firestore.FirebaseFirestore
-import kotlin.random.Random
+import android.widget.TextView
 
 class FightSelect : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //啟用自定義的主題
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fight_select)
-        val btq1 = findViewById<Button>(R.id.buttonQ1)
+        val btQ1 = findViewById<Button>(R.id.buttonQ1)
+        val title : TextView = findViewById(R.id.title)
+        title.setTextAppearance(R.style.AppTheme)
 
-        val questionDataCollectionName = "qtype"
-        val questionDataQuestionField = "Question"
-        val questionDataAnswerField = "answer"
-
-        val db = FirebaseFirestore.getInstance()
-        // Create a new document with a generated ID
-        val questionNumber = Random.nextInt(10) + 1
-        val QuestionNumber =
-            db.collection(questionDataCollectionName).document(questionNumber.toString())
-        val readDocRed = db.collection(questionDataCollectionName)
-
-        btq1.setOnClickListener {
+        btQ1.setOnClickListener {
             val intent = Intent(this, FightMain::class.java)
             startActivity(intent)
 
-            readDocRed.whereEqualTo(
-                questionDataQuestionField,
-                questionDataAnswerField.toString()
-            ).get()
-                .addOnSuccessListener { documents ->
-                    if (documents.size() > 0) {
-
-                        val Question = documents.first()
-                        val answer = Question.getString(questionDataAnswerField)
-
-                    }
-                }
         }
     }
 }
