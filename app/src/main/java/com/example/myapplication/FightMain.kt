d58263794638c382a8d724e460cad954c727870c
BoBo <bensonrohe67@gmail.com>
2023-05-18 15:26:00 +0800

    fix:修補錯誤訊息

--- app/src/main/java/com/example/myapplication/FightMain.kt
+++ app/src/main/java/com/example/myapplication/FightMain.kt
@@ -2,13 +2,13 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 
 class FightMain : AppCompatActivity() {
