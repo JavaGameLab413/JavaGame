7943db393586d152951db2d5149f3712426f5f26
starleave1218 <starleave1218@gmail.com>
2023-07-25 00:48:29 +0800

    feat:添加書本頁面及修改動畫

--- app/src/main/java/com/example/myapplication/Record.kt
+++ app/src/main/java/com/example/myapplication/Record.kt
@@ -20,13 +20,13 @@ class Record : AppCompatActivity() {
         // Animate layout1 to the right and layout2 to the left
         fun animateToLeft() {
             ObjectAnimator.ofFloat(layout1, "translationX", -800f).apply {
-                duration = 5000
+                duration = 4000
                 interpolator = AccelerateDecelerateInterpolator()
                 start()
             }
 
             ObjectAnimator.ofFloat(layout2, "translationX", 0f).apply {
-                duration = 4500
+                duration = 4000
                 interpolator = AccelerateDecelerateInterpolator()
                 start()
             }
@@ -35,13 +35,13 @@ class Record : AppCompatActivity() {
         // Animate layout1 to the left and layout2 to the right
         fun animateToRight() {
             ObjectAnimator.ofFloat(layout1, "translationX", 0f).apply {
-                duration = 4500
+                duration = 4000
                 interpolator = AccelerateDecelerateInterpolator()
                 start()
             }
 
             ObjectAnimator.ofFloat(layout2, "translationX", 800f).apply {
-                duration = 5000
+                duration = 4000
                 interpolator = AccelerateDecelerateInterpolator()
                 start()
             }
