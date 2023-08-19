64bdf67045e15d09adac3c8850d8624bfe7ae573
starleave1218 <starleave1218@gmail.com>
2023-08-19 23:39:58 +0800

    feat:新增逐字動畫

--- app/src/main/java/com/example/myapplication/Dialog.kt
+++ app/src/main/java/com/example/myapplication/Dialog.kt
@@ -2,12 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 
 class Dialog : AppCompatActivity(){
+    private lateinit var textView: TextView
+    private val textToDisplay = "我是一段超級長的測試文字，我主要用來測試逐字動畫還有TextView的寬度，但是我還是有一點點問題，所以暫時無法實用。"
+    private val delayInMillis = 50L
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog)
+        val speakName = findViewById<TextView>(R.id.name)
+        speakName.text="小狸"
+        textView = findViewById(R.id.context)
+        animateTextWithHandler()
+    }
+    private fun animateTextWithHandler() {
+        val handler = Handler(Looper.getMainLooper())
+        var charIndex = 0
+
+        handler.post(object : Runnable {
+            override fun run() {
+                if (charIndex < textToDisplay.length) {
+                    textView.text = textToDisplay.substring(0, charIndex + 1)
+                    charIndex++
+                    handler.postDelayed(this, delayInMillis) // Delay between characters
+                }
+            }
+        })
+
     }
 }
