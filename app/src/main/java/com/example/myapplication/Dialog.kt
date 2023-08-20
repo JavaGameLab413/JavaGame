52dbed1933f087d4014b107b8a130ebc6a106b4d
starleave1218 <starleave1218@gmail.com>
2023-08-20 14:58:46 +0800

    feat:點選對話框可以直接結束當前對話或進入下一段

--- app/src/main/java/com/example/myapplication/Dialog.kt
+++ app/src/main/java/com/example/myapplication/Dialog.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.coroutines.*
 
@@ -12,6 +13,7 @@ class Dialog : AppCompatActivity(){
     private lateinit var textView: TextView
     private val textToDisplay = "我是一段超級長的測試文字，我主要用來測試逐字動畫還有TextView的寬度，但是我還是有一點點問題，所以暫時無法實用。"
     private val delayInMillis = 50L
+    private var isRunning = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +23,35 @@ class Dialog : AppCompatActivity(){
         speakName.text="小狸"
         textView = findViewById(R.id.context)
         animateTextWithHandler()
+        val chat :ImageView = findViewById(R.id.chat)
+        chat.setOnClickListener {
+            if(!isRunning){
+                animateTextWithHandler()
+            }
+            else{
+                textView.text = textToDisplay
+                isRunning=false
+
+            }
+        }
+
+
     }
     private fun animateTextWithHandler() {
         val handler = Handler(Looper.getMainLooper())
         var charIndex = 0
-
+        isRunning = true
         handler.post(object : Runnable {
             override fun run() {
-                if (charIndex < textToDisplay.length) {
-                    textView.text = textToDisplay.substring(0, charIndex + 1)
-                    charIndex++
-                    handler.postDelayed(this, delayInMillis) // Delay between characters
+
+                if (isRunning) {
+                    if (charIndex < textToDisplay.length) {
+                        textView.text = textToDisplay.substring(0, charIndex + 1)
+                        charIndex++
+                        handler.postDelayed(this, delayInMillis) // Delay between characters
+                    } else {
+                        isRunning = false
+                    }
                 }
             }
         })
