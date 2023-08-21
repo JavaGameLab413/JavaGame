61c466c625bf70be23dd27d975baefa219ed57e9
terry <apple7235266@gmail.com>
2023-08-21 17:29:23 +0800

    feat: change dataset and add some annotation

--- app/src/main/java/com/example/myapplication/FightMain.kt
+++ app/src/main/java/com/example/myapplication/FightMain.kt
@@ -23,25 +23,28 @@ class FightMain : AppCompatActivity() {
     private var enemyAnimator: ObjectAnimator? = null
     private lateinit var enemyHp: ProgressBar
     private lateinit var playerHp: ProgressBar
-
+    private var dataSet = ""
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fight_main)
+        //畫面中四種選項的按鈕
         val btOptionsA = findViewById<Button>(R.id.OptionsA)
         val btOptionsB = findViewById<Button>(R.id.OptionsB)
         val btOptionsC = findViewById<Button>(R.id.OptionsC)
         val btOptionsD = findViewById<Button>(R.id.OptionsD)
         val correctOutput = "答案正確!"
         val errorOutput = "答案錯誤!"
-
-        enemyHp = findViewById(R.id.enemyHp)
-        playerHp = findViewById(R.id.playerHp)
-        enemyHp.progress = enemyHp.max
-        playerHp.progress = playerHp.max
+        dataSet = intent.getStringExtra("questionTitle").toString()
+        Log.d(TAG, "DataSet : $dataSet")        //測試顯示資料庫是讀取哪一個
+        enemyHp = findViewById(R.id.enemyHp)//敵對血條
+        playerHp = findViewById(R.id.playerHp)//我方血條
+        enemyHp.progress = enemyHp.max //設定值在設定畫面的設定檔中，目前設置為6
+        playerHp.progress = playerHp.max //設定值在設定畫面的設定檔中，目前設置為6
+        //設置選項按下去的行為
         btOptionsA.setOnClickListener {
-            if (answer == "a") {
+            if (answer == "SelectA") {
                 Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "The correct answer!")
 
@@ -54,7 +57,7 @@ class FightMain : AppCompatActivity() {
             checkFinish()
         }
         btOptionsB.setOnClickListener {
-            if (answer == "b") {
+            if (answer == "SelectB") {
                 Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "The correct answer!")
                 correct()
@@ -66,7 +69,7 @@ class FightMain : AppCompatActivity() {
             checkFinish()
         }
         btOptionsC.setOnClickListener {
-            if (answer == "c") {
+            if (answer == "SelectC") {
                 Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "The correct answer!")
                 correct()
@@ -78,7 +81,7 @@ class FightMain : AppCompatActivity() {
             checkFinish()
         }
         btOptionsD.setOnClickListener {
-            if (answer == "d") {
+            if (answer == "SelectD") {
                 Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "The correct answer!")
                 correct()
@@ -107,12 +110,12 @@ class FightMain : AppCompatActivity() {
         val btOptionsC = findViewById<Button>(R.id.OptionsC)
         val btOptionsD = findViewById<Button>(R.id.OptionsD)
         val mainQuestion = findViewById<TextView>(R.id.question)
-        val questionTypeDatabaseCollectionName = "questionsType"
         val db = FirebaseFirestore.getInstance()
-        val collectionRef = db.collection(questionTypeDatabaseCollectionName)
+        val collectionRef = db.collection(dataSet)
 
         //自定義的字體
         mainQuestion.setTextAppearance(R.style.AppTheme)
+
         // 使用get()方法取得集合中所有的文檔快照
         collectionRef.get()
             .addOnSuccessListener { documents ->
@@ -123,13 +126,13 @@ class FightMain : AppCompatActivity() {
                 // 取得指定索引的文檔
                 val randomDocument = documents.documents[randomIndex]
                 // 從文檔中讀取欄位值
-                val question = randomDocument.getString("Question")
-                val aOption = randomDocument.getString("a")
-                val bOption = randomDocument.getString("b")
-                val cOption = randomDocument.getString("c")
-                val dOption = randomDocument.getString("d")
-                answer = randomDocument.getString("ans").toString()
-
+                val question = randomDocument.getString("Info")
+                val aOption = randomDocument.getString("SelectA")
+                val bOption = randomDocument.getString("SelectB")
+                val cOption = randomDocument.getString("SelectC")
+                val dOption = randomDocument.getString("SelectD")
+                answer = randomDocument.getString("Answer").toString()
+                Log.d(TAG, "Question : $question")
                 // TODO: 使用讀取到的變數
                 mainQuestion.text = question.toString()
                 btOptionsA.text = aOption.toString()
@@ -198,7 +201,7 @@ class FightMain : AppCompatActivity() {
     private fun startAnimation(){
         enemyAnimator?.cancel()
 
-      val enemy: ImageView = findViewById(R.id.enemy)
+        val enemy: ImageView = findViewById(R.id.enemy)
         enemyAnimator = ObjectAnimator.ofFloat(enemy, "translationX", -15f, 15f)
             .apply {
                 duration = 200
