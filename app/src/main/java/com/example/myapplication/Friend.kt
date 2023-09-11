897dd33431f195ac27a3931150d092699d892233
starleave1218 <starleave1218@gmail.com>
2023-09-11 23:43:09 +0800

    feat:動態添加好友資訊組件

--- app/src/main/java/com/example/myapplication/Friend.kt
+++ app/src/main/java/com/example/myapplication/Friend.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class Friend: AppCompatActivity() {
@@ -8,5 +9,17 @@ class Friend: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend)
 
+        val linearLayout = findViewById<LinearLayout>(R.id.friends)
+
+
+        // 创建多个 CustomImageViewTextView 对象并设置属性
+        for (i in 1..5) {
+            val customView = FriendInfo(this, null)
+            customView.setImageResource(R.drawable.head2)
+            customView.setText("Item","Lv $i")
+
+            // 添加 CustomImageViewTextView 到 LinearLayout 中
+            linearLayout.addView(customView)
+        }
     }
 }
