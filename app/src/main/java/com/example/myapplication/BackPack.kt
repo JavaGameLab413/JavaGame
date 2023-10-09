db3efcb8e369d0368df9a9c09fc13446da45d4cf
starleave1218 <starleave1218@gmail.com>
2023-10-09 20:20:39 +0800

    feat:背包布局

--- app/src/main/java/com/example/myapplication/BackPack.kt
+++ app/src/main/java/com/example/myapplication/BackPack.kt
@@ -1,38 +1,44 @@
 package com.example.myapplication
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets.Type.navigationBars
-import android.view.WindowInsets.Type.statusBars
+import android.view.Gravity
+import android.widget.LinearLayout
 
 class BackPack : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_back_pack)
+
+        val scrollViewLayout = findViewById<LinearLayout>(R.id.ItemList)
+        val scrollViewLayout1 = findViewById<LinearLayout>(R.id.ItemList1)
+        val scrollViewLayout2 = findViewById<LinearLayout>(R.id.ItemList2)
+
+        for (i in 1..5) {
+            val customView = BackpackItems(this, null)
+            customView.setImageResource(R.drawable.healing_potion)
+            val customView1 = BackpackItems(this, null)
+            customView1.setImageResource(R.drawable.powerup)
+            val customView2 = BackpackItems(this, null)
+            customView2.setImageResource(R.drawable.healing_potion)
+
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.gravity = Gravity.CENTER
+            layoutParams.bottomMargin = 20
+
+            // 添加 CustomImageViewTextView 到 ScrollView 的直接子视图中
+            customView.layoutParams = layoutParams
+            customView1.layoutParams = layoutParams
+            customView2.layoutParams = layoutParams
+
+            scrollViewLayout.addView(customView)
+            scrollViewLayout1.addView(customView1)
+            scrollViewLayout2.addView(customView2)
+        }
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        val window = this.window
 
-        val decorView = window.decorView
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            window.insetsController?.also {
-                it.hide(statusBars())
-                it.hide(navigationBars())
-            }
-
-        }
-        else {
-            // 如果设备不支持 WindowInsetsController，则可以尝试使用旧版方法  <版本低於Android 11>
-            decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-        }
-    }
 }
