3099def84d19a48b65f8c5d4ded7cb6158420a4c
starleave1218 <132370823+starleave1218@users.noreply.github.com>
2023-11-19 23:09:08 +0800

    Equipment column (#75)
    * feat:修改註冊時名稱重複沒使用者提示
    
    * feat:註冊時PlayerInfo增加裝備中的裝備跟擁有的稱號
    
    * feat:稱號隨資料庫變動
    
    * feat:玩家資訊欄稱號隨資料庫變動
    
    * feat:修正裝備欄顯示問題
    
    * feat:讀取裝備中的裝備
    
    * feat:創建稱號欄位並可以刷新
    
    * feat:頁面結束後寫入資料庫
    
    * feat:裝備欄變組件
    
    * feat:更改背景
    
    * feat:戰鬥畫面添加裝備欄
    
    * feat:戰鬥畫面添加裝備欄畫面
    
    * feat:戰鬥畫面可使用藥水及修改程式碼
    
    * feat:使用藥水後資料庫會變動
    
    * feat:當使用後數量為0時的處理
    
    * feat:修正bug
    
    * feat:修正bug

--- app/src/main/java/com/example/myapplication/FightMain.kt
+++ app/src/main/java/com/example/myapplication/FightMain.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,13 +19,24 @@ class FightMain : AppCompatActivity() {
     private var bossLevelSet = ""
     private val db = FirebaseFirestore.getInstance()
 
+    private val map: Map<String, Int> =
+        mapOf("M1" to R.drawable.healing_potion, "M2" to R.drawable.powerup1) //物品圖片位置
+    private var equipmentNum = ArrayList<String>(5) //裝備中的物品名稱
 
+    private var userHp=0
+    private var userAtk=0
+    private var bossHp=0
+    private var bossAtk=0
+    private var tampAtk=0
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fight_main)
+
+        getEquipment()
+
         //畫面中四種選項的按鈕
         val btOptionsA = findViewById<Button>(R.id.OptionsA)
         val btOptionsB = findViewById<Button>(R.id.OptionsB)
@@ -35,43 +47,18 @@ class FightMain : AppCompatActivity() {
         //Log.d("Dataset", "DataSet : $dataSet")
         bossLevelSet = intent.getStringExtra("bossLevel").toString()
 
-        val bossDocumentRef = db.collection("Boss").document(bossLevelSet)
+        //裝備
+        val equipment1: ImageButton = findViewById(R.id.equipment1)
+        val equipment2: ImageButton = findViewById(R.id.equipment2)
+        val equipment3: ImageButton = findViewById(R.id.equipment3)
+        val equipment4: ImageButton = findViewById(R.id.equipment4)
+        val equipment5: ImageButton = findViewById(R.id.equipment5)
 
-        val sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
-        val userId =sharedPreferences.getString("ID", "-1").toString()
-        val playerInfoRef = db.collection("PlayerInfo").document(userId)
-        playerInfoRef.get().addOnSuccessListener { document ->
-            val userLevel: Int =
-                Integer.parseInt(document.getLong("Level").toString())
+        getValue()
 
-            val userDocumentRef = db.collection("Level").document(userLevel.toString())
-            bossDocumentRef.get()
-                .addOnSuccessListener { bossDocumentSnapshot ->
-                    if (bossDocumentSnapshot.exists()) {
-                        val bossHp: Int =
-                            Integer.parseInt(bossDocumentSnapshot.getLong("healthPoint").toString())
-                        userDocumentRef.get()
-                            .addOnSuccessListener { userDocumentSnapshot ->
-                                if (userDocumentSnapshot.exists()) {
-                                    val userHp: Int = Integer.parseInt(
-                                        userDocumentSnapshot.getLong("HP").toString()
-                                    )
-
-                                    enemyHp = findViewById(R.id.enemyHp)//敵對血條
-                                    playerHp = findViewById(R.id.playerHp)//我方血條
-                                    enemyHp.max = bossHp
-                                    playerHp.max = userHp
-                                    enemyHp.progress = enemyHp.max //設定值在設定畫面的設定檔中，目前設置為6
-                                    playerHp.progress = playerHp.max //設定值在設定畫面的設定檔中，目前設置為6
-                                }
-                            }
-                    }
-                }
-        }
         //設置選項按下去的行為
         btOptionsA.setOnClickListener {
             checkChoiceIsAns("SelectA")
-            Toast.makeText(this, "123", Toast.LENGTH_SHORT).show()
         }
         btOptionsB.setOnClickListener {
             checkChoiceIsAns("SelectB")
@@ -82,6 +69,23 @@ class FightMain : AppCompatActivity() {
         btOptionsD.setOnClickListener {
             checkChoiceIsAns("SelectD")
         }
+        //裝備
+        equipment1.setOnClickListener {
+            usePotion(equipment1.tag.toString())
+        }
+        equipment2.setOnClickListener {
+            usePotion(equipment2.tag.toString())
+        }
+        equipment3.setOnClickListener {
+            usePotion(equipment3.tag.toString())
+        }
+        equipment4.setOnClickListener {
+            usePotion(equipment3.tag.toString())
+        }
+        equipment5.setOnClickListener {
+            usePotion(equipment5.tag.toString())
+        }
+
     }
 
     //每次更新會做的事情，固定放在onCreate下方，其他方法往下放
@@ -132,60 +136,27 @@ class FightMain : AppCompatActivity() {
 
     private fun checkChoiceIsAns(btn: String) {
 
-        val bossDocumentRef = db.collection("Boss").document(bossLevelSet)
+        val correctOutput = "答案正確!"
+        val errorOutput = "答案錯誤!"
+        if (userHp > 0 && bossHp > 0) {
+            if (answer == btn) {
+                Log.d(TAG, "Boss HP: $bossHp")
+                Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT)
+                    .show()
+                enemyHp.progress -= (userAtk+tampAtk)
+                tampAtk = 0
+                correct()
+            } else {
+                Log.d(TAG, "User HP: $userHp")
+                Toast.makeText(this, errorOutput, Toast.LENGTH_SHORT)
+                    .show()
+                playerHp.progress -= bossAtk
+                tampAtk = 0
+            }
+            checkFinish()
 
-        val sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
-        val userId =sharedPreferences.getString("ID", "-1").toString()
-        val playerInfoRef = db.collection("PlayerInfo").document(userId)
-        playerInfoRef.get().addOnSuccessListener { document ->
-            val userLevel: Int =
-                Integer.parseInt(document.getLong("Level").toString())
-
-            val userDocumentRef = db.collection("Level").document(userLevel.toString())
-            bossDocumentRef.get()
-                .addOnSuccessListener { bossDocumentSnapshot ->
-                    if (bossDocumentSnapshot.exists()) {
-                        // 从文档中读取数据
-                        val bossHp: Int =
-                            Integer.parseInt(bossDocumentSnapshot.getLong("healthPoint").toString())
-                        val bossAttack: Int =
-                            Integer.parseInt(bossDocumentSnapshot.getLong("combatPower").toString())
-
-                        userDocumentRef.get()
-                            .addOnSuccessListener { userDocumentSnapshot ->
-                                if (userDocumentSnapshot.exists()) {
-                                    // 从文档中读取数据
-                                    val userHp: Int = Integer.parseInt(
-                                        userDocumentSnapshot.getLong("HP").toString()
-                                    )
-                                    val userAttack: Int = Integer.parseInt(
-                                        userDocumentSnapshot.getLong("Attack").toString()
-                                    )
-
-                                    val correctOutput = "答案正確!"
-                                    val errorOutput = "答案錯誤!"
-                                    if (userHp > 0 && bossHp > 0) {
-                                        if (answer == btn) {
-                                            Log.d(TAG, "Boss HP: $bossHp")
-                                            Toast.makeText(this, correctOutput, Toast.LENGTH_SHORT)
-                                                .show()
-                                            enemyHp.progress -= userAttack
-                                            correct()
-                                        } else {
-                                            Log.d(TAG, "User HP: $userHp")
-                                            Toast.makeText(this, errorOutput, Toast.LENGTH_SHORT)
-                                                .show()
-                                            playerHp.progress -= bossAttack
-                                        }
-                                        checkFinish()
-
-                                    }
-
-                                }
-                            }
-                    }
-                }
         }
+
     }
 
     private fun checkLevel() {
@@ -282,6 +253,199 @@ class FightMain : AppCompatActivity() {
             }
         enemyAnimator?.start()
     }
+
+    //從資料庫取得裝備中的物品
+    private fun getEquipment() {
+        clearEquipment()
+
+        val playerInfoDatabaseCollectionName = "PlayerInfo"
+        val sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection(playerInfoDatabaseCollectionName)
+            .document(sharedPreferences.getString("ID", "-1").toString())
+
+        docRef.get().addOnSuccessListener {doc ->
+            val text = doc.getString("Equipment")
+            if (text != null) {
+                for(title in text.split(Regex(","))){
+                    if(title!=""&&!(equipmentNum.contains(title))){
+                        equipmentNum.add(title)
+                    }
+
+                }
+                showWearEquipment()
+            }
+
+        }
+
+    }
+
+    private fun showWearEquipment(){
+
+        //裝備顯示
+        var count = 1
+        for (i in equipmentNum) {
+            when (count) {
+                1 -> {
+                    val equipmentId = map[i]
+                    if (equipmentId != null) {
+                        showEquipment(equipmentId, R.id.equipment1, i)
+                    }
+                    count++
+                }
+                2 -> {
+                    val equipmentId = map[i]
+                    if (equipmentId != null) {
+                        showEquipment(equipmentId, R.id.equipment2, i)
+                    }
+                    count++
+                }
+                3 -> {
+                    val equipmentId = map[i]
+                    if (equipmentId != null) {
+                        showEquipment(equipmentId, R.id.equipment3, i)
+                    }
+                    count++
+                }
+                4 -> {
+                    val equipmentId = map[i]
+                    if (equipmentId != null) {
+                        showEquipment(equipmentId, R.id.equipment4, i)
+                    }
+                    count++
+                }
+                5 -> {
+                    val equipmentId = map[i]
+                    if (equipmentId != null) {
+                        showEquipment(equipmentId, R.id.equipment5, i)
+                    }
+                    count++
+                }
+
+            }
+        }
+    }
+
+    //裝備後顯示
+    private fun showEquipment(id: Int, viewId: Int, tag: String) {
+        val equipment = findViewById<ImageButton>(viewId)
+        equipment.tag = tag
+        equipment.setImageResource(id)
+        equipment.visibility = View.VISIBLE
+    }
+
+    //取得玩家、BOSS數值
+    private fun getValue(){
+        dataSet = intent.getStringExtra("questionTitle").toString()
+        Log.d(TAG, "DataSet : $dataSet")
+        bossLevelSet = intent.getStringExtra("bossLevel").toString()
+
+        val bossDocumentRef = db.collection("Boss").document(bossLevelSet)
+
+        val sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+        val userId =sharedPreferences.getString("ID", "-1").toString()
+        val playerInfoRef = db.collection("PlayerInfo").document(userId)
+        playerInfoRef.get().addOnSuccessListener { document ->
+            val userLevel: Int =
+                Integer.parseInt(document.getLong("Level").toString())
+
+            val userDocumentRef = db.collection("Level").document(userLevel.toString())
+            bossDocumentRef.get()
+                .addOnSuccessListener { bossDocumentSnapshot ->
+                    if (bossDocumentSnapshot.exists()) {
+                        bossHp=Integer.parseInt(bossDocumentSnapshot.getLong("healthPoint").toString())
+                        bossAtk=Integer.parseInt(bossDocumentSnapshot.getLong("combatPower").toString())
+                        userDocumentRef.get()
+                            .addOnSuccessListener { userDocumentSnapshot ->
+                                if (userDocumentSnapshot.exists()) {
+                                    userHp= Integer.parseInt(userDocumentSnapshot.getLong("HP").toString()                                    )
+                                    userAtk=Integer.parseInt(userDocumentSnapshot.getLong("Attack").toString())
+                                    enemyHp = findViewById(R.id.enemyHp)//敵對血條
+                                    playerHp = findViewById(R.id.playerHp)//我方血條
+                                    enemyHp.max = bossHp
+                                    playerHp.max = userHp
+                                    enemyHp.progress = enemyHp.max //設定值在設定畫面的設定檔中，目前設置為6
+                                    playerHp.progress = playerHp.max //設定值在設定畫面的設定檔中，目前設置為6
+                                }
+                            }
+                    }
+                }
+        }
+
+    }
+
+    private fun usePotion(s:String){
+
+        db.collection("Item").document(s).get().addOnSuccessListener { doc->
+            val text = doc.getString("Effect")
+            val name = doc.getString("Name")
+            if(text!=null){
+                val effect = text.split(Regex(" "))
+
+                Log.e("a",effect[0])
+                Log.e("a",effect[1])
+
+                when(effect[0]){
+                    "Hp"->{
+                        playerHp.progress += Integer.parseInt(effect[1])
+                        Toast.makeText(this, "已使用 $name", Toast.LENGTH_SHORT).show()
+                        deletePotion(s)
+                    }
+                    "Atk"->{
+                        if(tampAtk==0){
+                            tampAtk = Integer.parseInt(effect[1])
+                            Toast.makeText(this, "已使用 $name", Toast.LENGTH_SHORT).show()
+                            deletePotion(s)
+                        }
+                        else{
+                            Toast.makeText(this, "$name 每次攻擊只能用一個", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
+
+    private fun deletePotion(tag:String){
+        val sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+        val userId =sharedPreferences.getString("ID", "-1").toString()
+        val documentReference=db.collection("BackPage").document(userId)
+        val userReference=db.collection("PlayerInfo").document(userId)
+        documentReference.get().addOnSuccessListener {doc ->
+            var num = Integer.parseInt(doc.getLong(tag).toString())
+            num--
+            documentReference.update(tag,num)
+            if(num==0){
+                equipmentNum.remove(tag)
+                var equipment=""
+                //稱號寫入格式
+                for(i in equipmentNum){
+                    equipment +="$i,"
+                }
+                userReference.update("Equipment",equipment)
+                getEquipment()
+            }else{
+                getEquipment()
+            }
+
+        }
+
+    }
+
+    private fun clearEquipment(){
+        val wear = findViewById<LinearLayout>(R.id.wear)
+        for (i in 0 until wear.childCount){
+            val child: View = wear.getChildAt(i)
+            child.visibility=View.INVISIBLE
+        }
+    }
+
 }
 
 
