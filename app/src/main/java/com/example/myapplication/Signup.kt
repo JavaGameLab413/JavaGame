5ae4b46267c632b6ecde3b4c7b47eedeb3ef65ca
starleave1218 <starleave1218@gmail.com>
2023-12-08 01:48:14 +0800

    feat: 修正bug

--- app/src/main/java/com/example/myapplication/Signup.kt
+++ app/src/main/java/com/example/myapplication/Signup.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
